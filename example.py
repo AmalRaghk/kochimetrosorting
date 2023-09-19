8bceff9e732cbdced62b3ce54bb9201f982c8407
Amal Ragh K <amalraghk123@gmail.com>
2023-09-19 23:29:13 +0530

    done with data

---
+++ example.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data=pd.read_csv("./data.csv") #dataframe
+print(data.iloc[2])
+print(len(data))
--- insertion .py
+++ insertion .py
@@ -1,15 +1,16 @@
 #kochi metro card users fequent tranvelers list
-travel_count=[['aluva', 20], ['Pulinchodu', 40], ['Companypadi', 40], ['Ambattukavu', 50], ['Muttom', 60], ['Kalamassery', 70], ['Pathadipalam', 80], ['Edapally', 90], ['Changampuzha Park', 60], ['Palarivattom', 80], ['JLN Stadium', 40], ['Kaloor', 30], ['Town Hall', 20], ['MG Road', 90], ['Maharajas College', 10], ['Ernakulam South', 50], ['Kadavanthara', 90], ['Elamkulam', 20], ['Vyttila', 20], ['Thykoodam', 90], ['Petta', 60]]
-def insertion_sort(travel_count):
-    for j in range(1,len(travel_count)):
-        key=travel_count[j][1]
-        keyv=travel_count[j]
+import pandas as pd
+data=pd.read_csv("./data.csv") #dataframe
+def insertion_sort(data):
+    for j in range(1,len(data)):
+        key=data.iloc[j][2]
+        keyv=data.iloc[j]
         i=j-1
-        while i>=0 and travel_count[i][1]>key:
-            travel_count[i+1]=travel_count[i]
+        while i>=0 and data.iloc[i][2]>key:
+            data.iloc[i+1]=data.iloc[i]
             i-=1
-        travel_count[i+1]=keyv
-    return travel_count
+        data.iloc[i+1]=keyv
+    return data
 
-print(insertion_sort(travel_count))
+print(insertion_sort(data))
     
